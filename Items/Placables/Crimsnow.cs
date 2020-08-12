331dbe4e21cd691cd0fe1adcd36ff6703477c4b8
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-12 15:02:17 -0700

    Add files via upload

---
+++ Items/Placables/Crimsnow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using CommunityModJam.Tiles;
+
+namespace CommunityModJam.Items.Placables
+{
+    class Crimsnow : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 16;
+            item.height = 16;
+            item.maxStack = 999;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.createTile = ModContent.TileType<CrimsnowTile>();
+        }
+    }
+}
