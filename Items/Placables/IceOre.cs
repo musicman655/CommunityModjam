f10915d96dcb4d149b9ca26ff55e5fd4441324bd
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-15 00:20:57 -0700

    item ores

---
+++ Items/Placables/IceOre.cs
@@ -0,0 +1,35 @@
+﻿using CommunityModJam.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CommunityModJam.Items.Placables
+{
+    class IceOre : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Glaciris Ore");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 16;
+            item.height = 16;
+            item.maxStack = 999;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.rare = ItemRarityID.Green;
+            item.createTile = ModContent.TileType<IceOreTile>();
+        }
+    }
+}
