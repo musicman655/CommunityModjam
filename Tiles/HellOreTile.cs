c1193adaff854f47384e220f79c61e714ae530a4
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-15 00:21:28 -0700

    ore tiles

---
+++ Tiles/HellOreTile.cs
@@ -0,0 +1,39 @@
+﻿using CommunityModJam.Items.Placables;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CommunityModJam.Tiles
+{
+    class HellOreTile : ModTile
+    {
+		public override void SetDefaults()
+		{
+			TileID.Sets.Ore[Type] = true;
+			Main.tileSpelunker[Type] = true;
+			Main.tileValue[Type] = 410;
+			Main.tileShine2[Type] = true;
+			Main.tileShine[Type] = 975;
+			Main.tileMergeDirt[Type] = true;
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Elyrium Ore");
+			AddMapEntry(new Color(186, 24, 32), name);
+
+			dustType = 84;
+			drop = ModContent.ItemType<HellOre>();
+			soundType = SoundID.Tink;
+			soundStyle = 1;
+			mineResist = 5f;
+			minPick = 200;
+		}
+	}
+}
